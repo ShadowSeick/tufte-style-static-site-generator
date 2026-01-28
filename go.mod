b5034bf4bfcd9d05a525ec745ce8ca68188ebf4c
ShadowSeick <alcolka@gmail.com>
2026-01-28 20:02:33 +0100

    initial thoughts of site generator

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShadowSeick/etf-site-generator
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Doing some things here
+	// Before proceding, I have to think of many things about how to do the project
+	// X Read ETF CSS
+	// - Think on how to select
+	// 		- h1 / h2 / h3
+	// 		- sections
+	// 		- labels
+	// 		- images
+}
