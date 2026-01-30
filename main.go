4a6ffee1d36120633e3b92a85cb99ff36b71ff11
ShadowSeick <alcolka@gmail.com>
2026-01-30 09:06:00 +0100

    starting this awesome project

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
+import (
+    "bufio"
+    "fmt"
+    "os"
+		"io/fs"
+)
+
 func main() {
-	// Doing some things here
-	// Before proceding, I have to think of many things about how to do the project
-	// X Read ETF CSS
-	// - Think on how to select
-	// 		- h1 / h2 / h3
-	// 		- sections
-	// 		- labels
-	// 		- images
+	file, err := os.OpenFile("./example.md", os.O_RDONLY, fs.ModeDevice)
+	if err != nil {
+		fmt.Errorf("error opening the file: %w", err)
+		return
+	}
+	defer file.Close()
+
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		// This will read line by line sepparated with an enter, here I need to check for which types I need to parse and create
+		fmt.Println(scanner.Text())
+	}
 }
