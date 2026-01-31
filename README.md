3b698a7e21bbcaa9b79b9fd4e935f29493a1c3b4
ShadowSeick <alcolka@gmail.com>
2026-01-31 11:49:00 +0100

    add markdown and parse headers

---
+++ README.md
@@ -0,0 +1,9 @@
+# ETF Site Generator
+This is a tool to generate from markdown files static sites with tufte CSS. The idea is for blog pages, but it could be used for much more. What more? I don't know
+
+[Tufte CSS](https://edwardtufte.github.io/tufte-css/)
+[Markdown syntax](https://www.markdownguide.org/)
+## Translations
+- IDs will be generated as the markdown syntax denotes {#some-id}
+- Pounds will be translated 1 to 1 to headers. h1 / h2 / h3 tags will be taken from the markdown counterpart, just reading the number of pounds.
+
--- main.go
+++ main.go
@@ -5,7 +5,85 @@ import (
     "fmt"
     "os"
 		"io/fs"
+		"strings"
 )
+
+const (
+	template = `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title>%s</title>
+		<link rel="stylesheet" href="assets/tufte.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"
+	</head>
+	<body>
+		<article>
+		%s
+		</article>
+	</body>
+	</html>
+	`
+	section = `<section>%s</section>`
+)
+
+
+type MarkdownElement uint8
+
+const (
+	Header MarkdownElement = iota
+	Subheader
+	Paragraph
+	Link
+	Sidenote
+	Code
+	Image
+	Italic
+	Bold
+	MarkdownCount
+)
+
+var htmlString = [MarkdownCount]string {
+	Header: `<h%d id="%s">%s</h%d>`,
+	Subheader: `<p class="subtitle">%s</p>`,
+	Paragraph: `<p>%s</p>`,
+	Link: `<a href="%s">%s</a>`,
+	Sidenote: `<label for="sn-%s" class="margin-toggle sidenote-number"></label><input type="checkbox" id="sn-%s" class="margin-toggle"><span class="sidenote">%s</span>`,
+	Code: `<pre><code>%s</code></pre>`,
+	Image: `<figure src="%s">%s</figure>`,
+	Italic: `<em>%s</em>`,
+	Bold: `<b>%s</b>`,
+}
+
+func ParseLine(l string) string {
+	// Need only to check at first
+	// HEADER
+	headerNumber := strings.Count(string(l), "#")
+	if headerNumber > 0 {
+		// Get header
+		header := l[headerNumber+1:]
+		// Check for the id
+		var id string
+		startIndexID := strings.IndexRune(l, '{')
+		if startIndexID > headerNumber {
+			// Get the actual id
+			id = header[startIndexID-1:len(header)-1]
+			header = header[:startIndexID-3]
+		}
+		return fmt.Sprintf(htmlString[Header], headerNumber, id, header, headerNumber)
+	}
+	return ""
+
+	// Subheader
+	// Paragraph
+	// Link
+	// Sidenote
+	// Code
+	// Image
+}
+
+
 
 func main() {
 	file, err := os.OpenFile("./example.md", os.O_RDONLY, fs.ModeDevice)
@@ -19,7 +97,9 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		// This will read line by line sepparated with an enter, here I need to check for which types I need to parse and create
-		fmt.Println(scanner.Text())
+		// name := file.Name()
+		// Generate the proper HTML page from markdown
+		s := ParseLine(scanner.Text())
+		fmt.Println(s)
 	}
 }
