03a4e8780c1627baf9e75a6c812bfa7b6809efcc
ShadowSeick <alcolka@gmail.com>
2026-02-02 20:03:01 +0100

    little step by little step

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ const (
 		<meta charset="utf-8">
 		<title>%s</title>
 		<link rel="stylesheet" href="assets/tufte.css"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
 		<article>
@@ -25,7 +25,6 @@ const (
 	</body>
 	</html>
 	`
-	section = `<section>%s</section>`
 )
 
 
@@ -36,8 +35,10 @@ const (
 	Subheader
 	Paragraph
 	Link
-	Sidenote
+	SideNote
+	MarginNote
 	Code
+	InlineCode
 	Image
 	Italic
 	Bold
@@ -49,41 +50,82 @@ var htmlString = [MarkdownCount]string {
 	Subheader: `<p class="subtitle">%s</p>`,
 	Paragraph: `<p>%s</p>`,
 	Link: `<a href="%s">%s</a>`,
-	Sidenote: `<label for="sn-%s" class="margin-toggle sidenote-number"></label><input type="checkbox" id="sn-%s" class="margin-toggle"><span class="sidenote">%s</span>`,
+	SideNote: `<label for="sn-%s" class="margin-toggle sidenote-number"></label><input type="checkbox" id="sn-%s" class="margin-toggle"><span class="sidenote">%s</span>`,
+	MarginNote: ``,
 	Code: `<pre><code>%s</code></pre>`,
+	InlineCode: `<code>%s</code>`,
 	Image: `<figure src="%s">%s</figure>`,
 	Italic: `<em>%s</em>`,
 	Bold: `<b>%s</b>`,
 }
 
-func ParseLine(l string) string {
+type State uint8
+
+const (
+	Title State = iota
+	NewSection
+	InsideSection
+	StateCount
+)
+
+func ParseLine(line string) (string, State) {
 	// Need only to check at first
 	// HEADER
-	headerNumber := strings.Count(string(l), "#")
+	var state State
+	if line == "" {
+		return "", InsideSection
+	}
+	headerNumber := strings.Count(string(line), "#")
 	if headerNumber > 0 {
 		// Get header
-		header := l[headerNumber+1:]
+		header := line[headerNumber+1:]
 		// Check for the id
 		var id string
-		startIndexID := strings.IndexRune(l, '{')
+		startIndexID := strings.IndexRune(line, '{')
 		if startIndexID > headerNumber {
 			// Get the actual id
 			id = header[startIndexID-1:len(header)-1]
 			header = header[:startIndexID-3]
 		}
-		return fmt.Sprintf(htmlString[Header], headerNumber, id, header, headerNumber)
-	}
-	return ""
 
-	// Subheader
+		switch headerNumber {
+		case 1:
+			state = Title
+		case 2:
+			state = NewSection
+		case 3:
+			state = InsideSection
+		}
+
+		return fmt.Sprintf(htmlString[Header], headerNumber, id, header, headerNumber), state
+	}
+
+	// Subheader @subheader
+	
+	
+	// What can go to a paragraph?
+	// - Link
+	// - Inline code
+	// - Sidenote
+	
+	// What can go into images?
+	// - Margin note
+
+	// What will never go alone?
+	// - Side notes
+	// - Margin notes
+	
+	// Side note [*side-note](side note)
+	// Margin note [^margin-note](margin note)
+	// Code -- How it is done in Markdwon
+	// Image -- ![alt-text](filepath)
 	// Paragraph
-	// Link
-	// Sidenote
-	// Code
-	// Image
+	return fmt.Sprintf(htmlString[Paragraph], line), InsideSection
 }
 
 
+// Maybe this approach is too naive. The problem is that sometimes I like to add Introduction while other I don't. I need to put both and the only way is to build a structure around into
+// I need to make some building blocks around them, but that would complicate a lot things... I still need to thing how to do it.
 
 func main() {
 	file, err := os.OpenFile("./example.md", os.O_RDONLY, fs.ModeDevice)
@@ -96,10 +138,37 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	oldState := Title
+	var html strings.Builder
 	for scanner.Scan() {
 		// name := file.Name()
 		// Generate the proper HTML page from markdown
-		s := ParseLine(scanner.Text())
-		fmt.Println(s)
+		// Need to keep track of where am I. For that I can make some sort of state machine
+		
+		text, newState := ParseLine(scanner.Text())
+
+		switch newState {
+		case Title:
+			html.WriteString(text)
+		case NewSection:
+			if oldState == InsideSection {
+				html.WriteString("</section>")
+			}
+			html.WriteString("<section>")
+			html.WriteString(text)
+		case InsideSection:
+			if oldState == Title {
+				html.WriteString("<section>")
+			}
+			html.WriteString(text)
+		default:
+			panic("invalid state")
+		}
+		html.WriteString("\n")
+		oldState = newState
 	}
+	html.WriteString("</section>")
+
+
+	fmt.Print(html.String())
 }
